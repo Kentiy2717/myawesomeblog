7be2c10656d33cc9e7bbe96c17ff9f088751001e
Kentiy <Kentiy93@gmail.com>
2021-02-19 19:58:04 +0700

    + model blog and event

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    date = models.DateField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='event_images/')
 
--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Events
-
+from events.models import Events
 
 admin.site.register(Events)
